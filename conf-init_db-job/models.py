0993c46651f149e5d594a58ab28bfa9484047bd7
mauromatsudo <maurao.matsudo@gmail.com>
2021-10-14 19:29:19 -0300

    remove unecessary imports from models.py

--- conf-init_db-job/models.py
+++ conf-init_db-job/models.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv, path
-from os.path import abspath
-#from dotenv import load_dotenv
+from os import getenv
 
 def set_string():
-    #proj_env = abspath('/app/.env')
-    #load_dotenv(dotenv_path=proj_env)
     db_user_password = getenv('db_user_password')
     db_username = 'root'
     MYSQL_SERVICE_HOST = getenv("MYSQL_SERVICE_HOST")
@@ -15,7 +11,7 @@ def set_string():
     db_name = getenv('db_name')
     return f'mysql+pymysql://{db_username}:{db_user_password}@{MYSQL_SERVICE_HOST}:{MYSQL_DATABASE_PORT}/{db_name}'
 
-#db_name = getenv('db_name')
+
 connect_string = set_string()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{connect_string}'
@@ -23,10 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Contact(db.Model):
-
-
     __tablename__ = 'contacts'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     surname = db.Column(db.String(100), nullable=True)
